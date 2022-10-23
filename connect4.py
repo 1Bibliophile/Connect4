ee8ba68fd8033416676e00ed596bfb0d5c0d44a7
David Morgenstern <DavidtheConfident@gmail.com>
2022-10-23 16:43:12 -0500

    Checks Horizontal locations for win

--- connect4.py
+++ connect4.py
@@ -5,14 +5,14 @@ ROW_COUNT = 6
 COLUMN_COUNT = 7
 
 def create_board():
-    board = np.zeros((6, 7))
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
 
 def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT - 1][col] == 0
 
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
@@ -21,6 +21,15 @@ def get_next_open_row(board, col):
 
 def print_board(board):
     print(np.flip(board, 0))
+
+def winning_move(board, piece):
+    # Checks all the horizontal locations for win
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+3] == piece and board[r][c] == piece:
+                return True
+
+    
 
 
 board = create_board()
@@ -36,6 +45,10 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
+
+            if winning_move(board, 1):
+                print("Player 1 Wins! Congrats!")
+                game_over = True
 
     # Ask for Player 2 Input
     else:
