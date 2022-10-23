8708e102a8fc5bb76f42183c95ade6e43918afc8
David Morgenstern <DavidtheConfident@gmail.com>
2022-10-23 16:47:53 -0500

    Checks Vertical Locations for win

--- connect4.py
+++ connect4.py
@@ -26,9 +26,13 @@ def winning_move(board, piece):
     # Checks all the horizontal locations for win
     for c in range(COLUMN_COUNT - 3):
         for r in range(ROW_COUNT):
-            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+3] == piece and board[r][c] == piece:
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
                 return True
 
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
     
 
 
