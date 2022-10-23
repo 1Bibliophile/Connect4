612d78cb04feb2d2afd2f083c290973286c7da6d
David Morgenstern <DavidtheConfident@gmail.com>
2022-10-23 16:24:05 -0500

    Tests Drop Piece

--- connect4.py
+++ connect4.py
@@ -1,18 +1,24 @@
 from cgi import print_form
 import numpy as np
 
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
 def create_board():
     board = np.zeros((6, 7))
     return board
 
-def drop_piece():
-    pass
+def drop_piece(board, row, col, piece):
+    board[row][col] == piece
 
 def is_valid_location(board, col):
     return board[5][col] == 0
 
-def get_next_open_row():
-    pass
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
 
 board = create_board()
 turn = 0
@@ -23,9 +29,19 @@ while not game_over:
     if turn == 0:
         col = int(input("Player 1, make your selection (0-6): "))
 
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
+
     # Ask for Player 2 Input
     else:
         col = int(input("Player 2, make your selection (0-6): "))
+
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+
+    print(board)
 
     turn += 1
     turn = turn % 2
